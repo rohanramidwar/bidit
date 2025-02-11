3bc681c251e3a9a62467f7cf6b9ee52c9dbe3fc3
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2025-02-11 16:22:34 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
             {isLoading ? (
               <Loader />
             ) : activeAuctions.length === 0 ? (
-              <div className="text-white">No Active Auctions Found</div>
+              <div>No active auctions found</div>
             ) : (
               activeAuctions?.map((auction) => (
                 <Card key={auction?._id} auction={auction} status={true} />
@@ -49,7 +49,7 @@ const Home = () => {
             {isLoading ? (
               <Loader />
             ) : endedAuctions.length === 0 ? (
-              <div className="text-white">No Past Auctions Found</div>
+              <div>No past auctions found</div>
             ) : (
               endedAuctions?.map((auction) => (
                 <Card key={auction?._id} auction={auction} status={false} />
