b0d10b2816ee2b8a19dd878c4b07f84fe860ea38
Rohan Ramidwar <rohanramidwar71@gmail.com>
2025-02-11 15:58:29 +0530

    changes

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -40,7 +40,7 @@ const MyOrdersPage = () => {
   };
 
   return (
-    <div className="pt-9 px-4 pb-20 w-full flex justify-center">
+    <div className="pt-9 px-4 pb-20 w-full md:flex md:justify-start">
       <div className="space-y-4">
         <h1 className="text-lg font-semibold text-teal-700">My Orders</h1>
         <div className="grid grid-cols-1 gap-4">
